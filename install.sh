6e757606818c90f22587591cc3ba7cce690c5349
brian <@laskow26>
2018-07-28 15:43:07 -0400

    cron

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -s $PWD/sitrep /usr/bin/sitrep
 mkdir -p /root/bin/
 mkdir -p /var/log/sitrep
 cp -av $PWD/sitrep-cron /root/bin/
-echo "1/5 * * * * root /root/bin/sitrep-cron" >> /etc/crontab
+echo "*/3   *   *   *   *  /root/bin/sitrep-cron" >> /var/spool/cron/crontabs/root
